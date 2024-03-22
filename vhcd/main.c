6b53edb240de86c727a12df5517bb706a9cf6892
Yiwei Lin <s921975628@gmail.com>
2024-03-22 23:34:11 +0800

    Bind vhcd device with driver

--- vhcd/main.c
+++ vhcd/main.c
@@ -4,13 +4,38 @@
 #include <linux/platform_device.h>
 #include <linux/usb.h>
 
+#define DEV_NAME "vhcd-pdev"
+
 static void hcd_pdev_dummy_release(struct device *dev) {}
 
 static struct platform_device hcd_pdev = {
-    .name = "vhcd-pdev",
+    .name = DEV_NAME,
     .id = PLATFORM_DEVID_NONE,
     .dev.release = hcd_pdev_dummy_release,
 };
+
+static int hcd_probe(struct platform_device *pdev);
+static int hcd_remove(struct platform_device *pdev);
+static struct platform_driver hcd_driver = {
+    .probe = hcd_probe,
+    .remove = hcd_remove,
+    .driver =
+        {
+            .name = DEV_NAME,
+        },
+};
+
+static int hcd_probe(struct platform_device *pdev)
+{
+    pr_info("Probe hcd\n");
+    return 0;
+}
+
+static int hcd_remove(struct platform_device *pdev)
+{
+    pr_info("Remove hcd\n");
+    return 0;
+}
 
 static int __init hcd_pdev_init(void)
 {
@@ -20,9 +45,19 @@ static int __init hcd_pdev_init(void)
         return -ENODEV;
 
     ret = platform_device_register(&hcd_pdev);
+    if (ret < 0)
+        goto err_register_hcd_dev;
 
-    pr_info("Init hcd_pdev: %d\n", ret);
+    ret = platform_driver_register(&hcd_driver);
+    if (ret < 0)
+        goto err_register_hcd_driver;
 
+    pr_info("Initialize hcd: %d\n", ret);
+    return ret;
+
+err_register_hcd_driver:
+    platform_device_unregister(&hcd_pdev);
+err_register_hcd_dev:
     return ret;
 }
 module_init(hcd_pdev_init);
@@ -30,6 +65,8 @@ module_init(hcd_pdev_init);
 static void __exit hcd_pdev_exit(void)
 {
     platform_device_unregister(&hcd_pdev);
+    platform_driver_unregister(&hcd_driver);
+    pr_info("Exit hcd\n");
 }
 module_exit(hcd_pdev_exit);
 
