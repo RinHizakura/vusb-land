8cb45a7007ba1ca4ca7b7ce3b8efdb93f37221c2
Yiwei Lin <s921975628@gmail.com>
2024-03-25 00:14:47 +0800

    Implement basic flow for driver registered
    The remove flow of the driver is not complete yet, please
    don't try to use the module now.

--- vhcd/main.c
+++ vhcd/main.c
@@ -1,74 +1,265 @@
-#define pr_fmt(fmt) "[vhcd] " fmt
+#define pr_fmt(fmt) "vhcd " fmt
 
 #include <linux/module.h>
 #include <linux/platform_device.h>
+#include <linux/slab.h>
 #include <linux/usb.h>
+#include <linux/usb/hcd.h>
 
-#define DEV_NAME "vhcd-pdev"
+#define DEV_NAME "vhcd"
 
-static void hcd_pdev_dummy_release(struct device *dev) {}
-
-static struct platform_device hcd_pdev = {
-    .name = DEV_NAME,
-    .id = PLATFORM_DEVID_NONE,
-    .dev.release = hcd_pdev_dummy_release,
+/* This is the sturcture which will be referenced by usb_hcd->hcd_priv
+ * as private data */
+struct vhcd_hcd_priv {
+    struct vhcd_data *data;
 };
 
-static int hcd_probe(struct platform_device *pdev);
-static int hcd_remove(struct platform_device *pdev);
-static struct platform_driver hcd_driver = {
-    .probe = hcd_probe,
-    .remove = hcd_remove,
+static inline struct usb_hcd *priv_to_hcd(struct vhcd_hcd_priv *hcd_priv)
+{
+    return container_of((void *) hcd_priv, struct usb_hcd, hcd_priv);
+}
+
+/* This is the structure which will be referenced by
+ * platform_device->dev.platform_data, which is registered by
+ * platform_device_add_data(). */
+struct vhcd_data {
+    struct vhcd_hcd_priv *hs_hcd_priv;
+};
+
+static inline struct vhcd_data *get_platdata(struct platform_device *pdev)
+{
+    return *((void **) dev_get_platdata(&pdev->dev));
+}
+
+static void vhcd_pdev_dummy_release(struct device *dev) {}
+
+static struct platform_device vhcd_pdev = {
+    .name = DEV_NAME,
+    .id = PLATFORM_DEVID_NONE,
+    .dev.release = vhcd_pdev_dummy_release,
+};
+
+static int vhcd_setup(struct usb_hcd *hcd)
+{
+    struct vhcd_data *data;
+
+    pr_info("Setup hcd\n");
+
+    data = *((void **) dev_get_platdata(hcd->self.controller));
+    hcd->self.sg_tablesize = ~0;
+
+    data->hs_hcd_priv = (struct vhcd_hcd_priv *) (hcd->hcd_priv);
+    data->hs_hcd_priv->data = data;
+
+    hcd->speed = HCD_USB2;
+    hcd->self.root_hub->speed = USB_SPEED_HIGH;
+
+    return 0;
+}
+
+static int vhcd_start(struct usb_hcd *hcd)
+{
+    pr_info("Start hcd\n");
+    /* TODO: Start hcd with correct status. */
+    return 0;
+}
+
+static void vhcd_stop(struct usb_hcd *hcd)
+{
+    pr_info("Stop hcd\n");
+}
+
+int vhcd_urb_enqueue(struct usb_hcd *hcd, struct urb *urb, gfp_t mem_flags)
+{
+    return 0;
+}
+
+int vhcd_urb_dequeue(struct usb_hcd *hcd, struct urb *urb, int status)
+{
+    return 0;
+}
+
+int vhcd_h_get_frame(struct usb_hcd *hcd)
+{
+    return 0;
+}
+
+int vhcd_hub_status(struct usb_hcd *hcd, char *buf)
+{
+    return 0;
+}
+
+int vhcd_hub_control(struct usb_hcd *hcd,
+                     u16 typeReq,
+                     u16 wValue,
+                     u16 wIndex,
+                     char *buf,
+                     u16 wLength)
+{
+    return 0;
+}
+
+int vhcd_bus_suspend(struct usb_hcd *)
+{
+    return 0;
+}
+
+int vhcd_bus_resume(struct usb_hcd *)
+{
+    return 0;
+}
+
+int vhcd_alloc_streams(struct usb_hcd *hcd,
+                       struct usb_device *udev,
+                       struct usb_host_endpoint **eps,
+                       unsigned int num_eps,
+                       unsigned int num_streams,
+                       gfp_t mem_flags)
+{
+    return 0;
+}
+
+int vhcd_free_streams(struct usb_hcd *hcd,
+                      struct usb_device *udev,
+                      struct usb_host_endpoint **eps,
+                      unsigned int num_eps,
+                      gfp_t mem_flags)
+{
+    return 0;
+}
+
+static struct hc_driver vhcd_hc_driver = {
+    .description = DEV_NAME,
+    .product_desc = "vhcd host controller",
+    .hcd_priv_size = sizeof(struct vhcd_hcd_priv),
+
+    .reset = vhcd_setup,
+    .start = vhcd_start,
+    .stop = vhcd_stop,
+
+    .urb_enqueue = vhcd_urb_enqueue,
+    .urb_dequeue = vhcd_urb_dequeue,
+
+    .get_frame_number = vhcd_h_get_frame,
+
+    .hub_status_data = vhcd_hub_status,
+    .hub_control = vhcd_hub_control,
+    .bus_suspend = vhcd_bus_suspend,
+    .bus_resume = vhcd_bus_resume,
+
+    .alloc_streams = vhcd_alloc_streams,
+    .free_streams = vhcd_free_streams,
+};
+
+static int vhcd_probe(struct platform_device *pdev)
+{
+    struct vhcd_data *data;
+    struct usb_hcd *hs_hcd;
+    int ret;
+
+    pr_info("Probe vhcd\n");
+
+    // Simulate as an USB 2.0 device
+    vhcd_hc_driver.flags = HCD_USB2;
+
+    hs_hcd = usb_create_hcd(&vhcd_hc_driver, &pdev->dev, dev_name(&pdev->dev));
+    if (!hs_hcd)
+        return -ENOMEM;
+
+    /* Register usb_hcd after structure initialization. This will
+     * trigger the ->reset()/vhcd_setup and ->start()/vhcd_start
+     * callback. Note that ->start() is a must-needed callback,
+     * otherwise the kernel will crash without it. */
+    ret = usb_add_hcd(hs_hcd, 0, 0);
+    if (ret) {
+        usb_put_hcd(hs_hcd);
+        /* If usb_add_hcd() return error because ->reset() success but ->start()
+         * fail, this relationship could be set incorrectly. The driver needs to
+         * reset it. */
+        data = get_platdata(pdev);
+        data->hs_hcd_priv = NULL;
+        return ret;
+    }
+
+    return 0;
+}
+
+static int vhcd_remove(struct platform_device *pdev)
+{
+    struct vhcd_data *data;
+    pr_info("Remove vhcd\n");
+
+    data = get_platdata(pdev);
+
+    /* Shutdown usb_hcd by reverseing what we do at usb_add_hcd().
+     * This will trigger ->stop()/vhcd_stop which is a must-needed
+     * callback. */
+    usb_remove_hcd(priv_to_hcd(data->hs_hcd_priv));
+    usb_put_hcd(priv_to_hcd(data->hs_hcd_priv));
+
+    data->hs_hcd_priv = NULL;
+
+    return 0;
+}
+
+static struct platform_driver vhcd_driver = {
+    .probe = vhcd_probe,
+    .remove = vhcd_remove,
     .driver =
         {
             .name = DEV_NAME,
         },
 };
 
-static int hcd_probe(struct platform_device *pdev)
+static int __init hcd_init(void)
 {
-    pr_info("Probe hcd\n");
-    return 0;
-}
-
-static int hcd_remove(struct platform_device *pdev)
-{
-    pr_info("Remove hcd\n");
-    return 0;
-}
-
-static int __init hcd_pdev_init(void)
-{
+    struct vhcd_data *data = NULL;
     int ret;
 
     if (usb_disabled())
         return -ENODEV;
 
-    ret = platform_device_register(&hcd_pdev);
+    ret = platform_device_register(&vhcd_pdev);
     if (ret < 0)
         goto err_register_hcd_dev;
 
-    ret = platform_driver_register(&hcd_driver);
+    data = kzalloc(sizeof(struct vhcd_data), GFP_KERNEL);
+    if (!data) {
+        ret = -ENOMEM;
+        goto err_alloc_pdata;
+    }
+    ret = platform_device_add_data(&vhcd_pdev, &data, sizeof(void *));
+    if (ret) {
+        goto err_add_pdata;
+    }
+
+    ret = platform_driver_register(&vhcd_driver);
     if (ret < 0)
         goto err_register_hcd_driver;
 
-    pr_info("Initialize hcd: %d\n", ret);
+    pr_info("Initialize vhcd: %d\n", ret);
     return ret;
 
 err_register_hcd_driver:
-    platform_device_unregister(&hcd_pdev);
+err_add_pdata:
+    kfree(data);
+err_alloc_pdata:
+    platform_device_unregister(&vhcd_pdev);
+
 err_register_hcd_dev:
     return ret;
 }
-module_init(hcd_pdev_init);
+module_init(hcd_init);
 
-static void __exit hcd_pdev_exit(void)
+static void __exit hcd_exit(void)
 {
-    platform_device_unregister(&hcd_pdev);
-    platform_driver_unregister(&hcd_driver);
-    pr_info("Exit hcd\n");
+    struct vhcd_data *data = get_platdata(&vhcd_pdev);
+    kfree(data);
+    platform_device_unregister(&vhcd_pdev);
+    platform_driver_unregister(&vhcd_driver);
+    pr_info("Exit vhcd\n");
 }
-module_exit(hcd_pdev_exit);
+module_exit(hcd_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("RinHizakura");
