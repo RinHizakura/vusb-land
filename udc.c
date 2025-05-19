070fe54057b9c5188f5844418a2b6f718dd7aea8
Yiwei Lin <s921975628@gmail.com>
2025-05-20 02:02:48 +0800

    Initialize sysfs knobs for UDC controller

--- udc.c
+++ udc.c
@@ -20,6 +20,16 @@ static const struct usb_gadget_ops vudc_gadget_ops = {
     .udc_set_speed = vudc_set_speed,
 };
 
+static ssize_t function_show(struct device *dev,
+                             struct device_attribute *attr,
+                             char *buf)
+{
+    struct virt *virt = container_of(dev, struct virt, gadget.dev);
+
+    return 0;
+}
+static DEVICE_ATTR_RO(function);
+
 static int vudc_probe(struct platform_device *pdev)
 {
     int ret;
@@ -33,9 +43,22 @@ static int vudc_probe(struct platform_device *pdev)
     virt->gadget.ops = &vudc_gadget_ops;
     virt->gadget.name = UDC_DEV_NAME;
     virt->gadget.max_speed = USB_SPEED_HIGH;
+    virt->gadget.dev.parent = &pdev->dev;
 
     ret = usb_add_gadget_udc(&pdev->dev, &virt->gadget);
+    if (ret < 0)
+        goto err_add_gadget_udc;
 
+    ret = device_create_file(&virt->gadget.dev, &dev_attr_function);
+    if (ret < 0)
+        goto err_dev;
+    platform_set_drvdata(pdev, virt);
+
+    return 0;
+
+err_dev:
+    usb_del_gadget_udc(&virt->gadget);
+err_add_gadget_udc:
     return ret;
 }
 
@@ -45,6 +68,7 @@ static void vudc_remove(struct platform_device *pdev)
 
     INFO("Remove vudc\n");
 
+    device_remove_file(&virt->gadget.dev, &dev_attr_function);
     usb_del_gadget_udc(&virt->gadget);
 }
 
